485e5cd05c7258b790593346e4863c2a1ccf8472
Sloffcraft <80576394+Sloffcraft@users.noreply.github.com>
2021-03-15 08:34:35 +0530

    move changes

--- writing_prac.py
+++ writing_prac.py
@@ -15,6 +15,36 @@ pen.write("Hello There" , font = ("courier" , 14 , "bold" ))
 pen.speed(0)
 pen.hideturtle()
 
+#second object
+sq = turtle.Turtle()
+sq.shape("square")
+sq.penup()
+sq.speed(0)
+sq.color("black")
+sq.goto(0,-280)
+
+#functions of the player 
+def sq_up():
+	y = sq_up.ycor()
+	y += 20
+	sq_up.sety(y)
+	
+def sq_right():
+	x = sq.xcor()
+	x += 20
+	sq.setx(x)
+	
+def sq_left():
+	x = sq.xcor()
+	x -= 20
+	sq.setx(x)
+	
+#keyboard binding 
+wn.listen()
+wn.onkeypress(sq_up , "w")
+wn.onkeypress(sq_right , "d")
+wn.onkeypress(sq_left , "a")
+
 #loop
 while True :
     wn.update()
